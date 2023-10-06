554ad7a8735d6f52e107cc53f00f19b649ed8722
Andile Mbele <andilembele020@gmail.com>
2023-10-06 15:48:37 +0200

    debugging: error 'local variable 'key' referenced before assignment'

--- mergerelatedcolumns.py
+++ mergerelatedcolumns.py
@@ -27,54 +27,35 @@ def merge_csv_files(file1, file2):
 
     # Compare the dataframes to see if they have related columns in common.
     # Use either the id, userid, username or email column as the key for comparison.
-    if 'id' in df1.columns and 'id' in df2.columns:
-        if df1['id'].equals(df2['id']):
-            key = 'id'
-    elif 'id' in df1.columns and 'userid' in df2.columns:
-        if df1['id'].equals(df2['userid']):
-            key = 'userid'
-    elif 'userid' in df1.columns and 'id' in df2.columns:
-        if df1['userid'].equals(df2['id']):
-            key = 'id'
-    elif 'userid' in df1.columns and 'userid' in df2.columns:
-        if df1['userid'].equals(df2['userid']):
-            key = 'userid'
-    elif 'username' in df1.columns and 'username' in df2.columns:
-        if df1['username'].equals(df2['username']):
-            key = 'username'
-    elif 'email' in df1.columns and 'email' in df2.columns:
-        if df1['email'].equals(df2['email']):
-            key = 'email'
-    else:
-        print("The dataframes do not have related columns in common")
+    key = None
+    for col in df1.columns:
+        if col in df2.columns:
+            key = col
+            break
 
-    # for col in df1.columns:
-    #     if col in df2.columns:
-    #         key = col
-    #         break
-
-#     Check matching values in the rows. Find a secondary key to use for match search. Use email column or email
-#     regex as the key for secondary match search.
+    # Check matching values in the rows. Find a secondary key to use for match search. Use email column or email
+    # regex as the key for secondary match search.
+    secondary_key = None
     if 'email' in df1.columns and 'email' in df2.columns:
-        if df1['email'].equals(df2['email']):
-            secondary_key = 'email'
+        secondary_key = 'email'
     else:
         secondary_key = 'id'
 
     # Calculate the percentage of matching values
     matching_values = 0
-    for i in range(len(df1)):
-        for j in range(len(df2)):
-            if df1[key][i] == df2[secondary_key][j]:
-                matching_values += 1
-    percentage = (matching_values / len(df1)) * 100
+    if key is not None and secondary_key is not None:
+        for i in range(len(df1)):
+            for j in range(len(df2)):
+                if df1[key][i] == df2[secondary_key][j]:
+                    matching_values += 1
+        percentage = (matching_values / len(df1)) * 100
 
-    # If the percentage of matching values is greater than 50%, merge the dataframes. Use the id or userid column as
-    # the key.
-    if percentage > 50:
-        merged_df = pd.merge(df1, df2, on=key)
-        merged_df.to_csv('merged.csv', index=False)
-        print("The dataframes have been merged successfully")
+        # If the percentage of matching values is greater than 50%, merge the dataframes. Use the id or userid column as
+        # the key.
+        if percentage > 50:
+            merged_df = pd.merge(df1, df2, on=key)
+            merged_df.to_csv('merged.csv', index=False)
+            print("The dataframes have been merged successfully")
 
 if __name__ == '__main__':
     # Get two CSV files in the directory, the files can be passed as command line arguments or the program can prompt the user to enter the file names
