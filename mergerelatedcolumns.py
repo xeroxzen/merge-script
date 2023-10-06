cafdfa477a2a8e33eefd5fdf83daf48b2edddb0d
Google Jr <andilembele020@gmail.com>
2023-10-06 22:33:28 +0200

    feat: rewrite #15

--- mergerelatedcolumns.py
+++ mergerelatedcolumns.py
@@ -26,9 +26,9 @@ def merge_csv_files(file1, file2):
     if not common_columns:
         if ('user' in file1 or 'usermeta' in file1) and ('user' in file2 or 'usermeta' in file2):
             # If so, set the key column to "id" or "userid", whichever is present in both dataframes.
-            if 'id' in df1.columns and 'id' in df2.columns:
+            if 'id' in df1.columns and 'userid' in df2.columns:
                 key_column = 'id'
-            elif 'userid' in df1.columns and 'userid' in df2.columns:
+            elif 'userid' in df1.columns and 'id' in df2.columns:
                 key_column = 'userid'
             else:
                 print("Could not find a key column for merging the dataframes.")
@@ -37,28 +37,28 @@ def merge_csv_files(file1, file2):
             print(
                 "The dataframes do not have any common columns and do not have the words 'user' or 'usermeta' in their file names.")
             return None
+    # else:
+    #     # Check if "id" and "userid" are both present in the common columns, and choose one as the key column.
+    #     if 'id' in common_columns and 'userid' in common_columns:
+    #         key_column = 'id'  # You can choose 'userid' here if you prefer.
     else:
-        # Check if "id" and "userid" are both present in the common columns, and choose one as the key column.
-        if 'id' in common_columns and 'userid' in common_columns:
-            key_column = 'id'  # You can choose 'userid' here if you prefer.
-        else:
-            # Use the common column with the most matching values as the key column.
-            key_column = None
-            max_matching_values = 0
-            for column in common_columns:
-                matching_values = 0
-                for i in range(len(df1)):
-                    for j in range(len(df2)):
-                        if df1[column][i] == df2[column][j]:
-                            matching_values += 1
-                if matching_values > max_matching_values:
-                    max_matching_values = matching_values
-                    key_column = column
+        # Use the common column with the most matching values as the key column.
+        key_column = None
+        max_matching_values = 0
+        for column in common_columns:
+            matching_values = 0
+            for i in range(len(df1)):
+                for j in range(len(df2)):
+                    if df1[column][i] == df2[column][j]:
+                        matching_values += 1
+            if matching_values > max_matching_values:
+                max_matching_values = matching_values
+                key_column = column
 
-            # If a key column was not found, print an error message and return None.
-            if key_column is None:
-                print("Could not find a key column for merging the dataframes.")
-                return None
+        # If a key column was not found, print an error message and return None.
+        if key_column is None:
+            print("Could not find a key column for merging the dataframes.")
+            return None
 
     # Calculate the percentage of matching values between the two dataframes.
     matching_values = 0
