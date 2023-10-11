9c9bb6e1260dea1b1bcecb0bd87230438584e88f
Google Jr <andilembele020@gmail.com>
2023-10-11 23:22:47 +0200

    fix: some bug fix

--- merge.py
+++ merge.py
@@ -9,7 +9,6 @@ def merge_csv_files_in_directory(directory):
     # Create an empty dictionary to track dataframes by their filenames.
     dataframes = {}
 
-    # List of possible PII.
     allowed_pii_columns = ['email', 'phone', 'ssn', 'user_id', 'customer_id', 'userid', 'customer_email', 'username',
                            'phonenumber', 'phone_number', 'billing_phone', 'billing_phone_number',
                            'billing_phone_number', 'billing_email', 'billing_email_address', 'billing_emailaddress',
@@ -22,7 +21,6 @@ def merge_csv_files_in_directory(directory):
             file_path = os.path.join(directory, filename)
             df = pd.read_csv(file_path, encoding='utf-8')
 
-            # Identify common columns with allowed PII columns.
             common_columns = set(df.columns).intersection(allowed_pii_columns)
 
             if not common_columns:
@@ -65,11 +63,9 @@ def merge_csv_files_in_directory(directory):
         else:
             merged_df = pd.merge(merged_df, df, on=key_column)
 
-    if merged_df.duplicated(subset=key_column).any():
+    if merged_df is not None and merged_df.duplicated(subset=key_column).any():
         merged_df.drop_duplicates(subset=key_column, inplace=True)
 
-    # combine first and last name
-    # Combine first and last name if both exist
     if 'firstname' in merged_df.columns and 'lastname' in merged_df.columns:
         merged_df['fullname'] = merged_df['firstname'] + " " + merged_df['lastname']
         merged_df.drop(columns=['firstname', 'lastname'], inplace=True)
@@ -77,17 +73,14 @@ def merge_csv_files_in_directory(directory):
         merged_df['fullname'] = merged_df['first_name'] + " " + merged_df['last_name']
         merged_df.drop(columns=['first_name', 'last_name'], inplace=True)
 
-    # Drop usernicename if username exists.
-    if 'username' in merged_df.columns and 'usernicename' in merged_df.columns:
-        merged_df.drop(columns=['usernicename'], inplace=True)
+    if 'username' in merged_df.columns:
+        merged_df.drop(columns=['usernicename'], errors='ignore', inplace=True)
 
     # Remove duplicate columns with the same row contents.
     merged_df = merged_df.T.drop_duplicates().T
 
-    # Generate a random number and add it to the output filename.
     random_suffix = ''.join(random.choice(string.ascii_letters) for _ in range(4))
     output_filename = f"merged_{random_suffix}_{key_column}.csv"
-    # Save the merged dataframe to a CSV file.
     output_dir = os.path.join(directory, output_filename)
     merged_df.to_csv(output_dir, index=False)
 
@@ -95,11 +88,11 @@ def merge_csv_files_in_directory(directory):
         print(f"Merged dataframes successfully and saved as '{output_filename}'.")
         print(merged_df.head())
 
+    return merged_df
+
 if __name__ == "__main__":
-    # Start the timer.
     start = timeit.default_timer()
 
-    # Check if the user provided a directory as a command line argument.
     if len(sys.argv) != 2:
         print("Please provide a directory containing CSV files as a command line argument.")
         sys.exit()
@@ -112,6 +105,5 @@ if __name__ == "__main__":
 
     merge_csv_files_in_directory(directory)
 
-    # Stop the timer.
     stop = timeit.default_timer()
     print('Time: ', stop - start)
