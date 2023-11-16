c68cd73f686fbdcab8d0d330e95e54c65d47b836
Google Jr <andilembele020@gmail.com>
2023-11-16 18:23:42 +0200

    ... in tim

--- mergeandclean.py
+++ mergeandclean.py
@@ -8,8 +8,8 @@ import timeit
 from usermetacleaner import usermeta_cleaner
 
 
-def timestamp(length=4):
-    # Create timestamp tto use for unique filenames
+def random_string(length=4):
+    return ''.join(random.choices(string.ascii_letters, k=length))
     
 
 def find_cleaned_usermeta_file(directory, usermeta_filename):
